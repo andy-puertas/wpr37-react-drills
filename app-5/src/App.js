7fdbee5b090f8dedbbca3c161e1474e92213e5a0
Andy Puertas <andy.puertas08@gmail.com>
2018-05-30 23:59:12 -0600

    drill 5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -19,3 +19,22 @@ class App extends Component {
 }
 
 export default App;
+
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import Image from './Image';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <Image myImage={ "https://http.cat/409" } />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
